09a4f1740cbd8bd974210a8b2025629d692d8e91
rikicop <ruperto1@protonmail.com>
2021-07-16 16:38:06 -0400

    useeffect

--- src/index_dogs.js
+++ src/index_dogs.js
@@ -1,31 +1,31 @@
 import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+// 1. Import *useState* and *useEffect*
 
 function Imagen({ subimg }) {
   // 2. Create our *dogImage* variable as well as the *setDogImage* function via useState
-  
-  let [cuadroImage, setCuadroImage] = useState(null)
+  // We're setting the default value of dogImage to null, so that while we wait for the
+  // fetch to complete, we dont attempt to render the image
+  let [dogImage, setDogImage] = useState(null)
 
   // 3. Create out useEffect function
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/photos/${subimg}`)
+    fetch(`https://dog.ceo/api/breeds/image/random/${subimg}`)
       .then(response => response.json())
-      // 4. Setting *Image* to the image url that we received from the response above
-      .then(data => {
-        setCuadroImage(data.url)
-        console.log("Data?: ", data.url)
-      })
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then(data => setDogImage(data.message))
   }, [subimg])
 
   return (
     <div className="App">
       {/* 5. Returning an img element for each url, again with the value of our src set to the image url */}
-      {<img
+      {dogImage && dogImage.map(
+        (dog) => <img
           width={"200px"}
           height={"200px"}
-          src={cuadroImage}
+          src={dog}
           alt="Perritos">
-        </img>}
+        </img>)}
     </div>
   );
 }
@@ -33,7 +33,7 @@ function Imagen({ subimg }) {
 function App() {
   // 2 pieces of state: one to hold the input value,
   // another to hold the current subreddit.
-  const [inputValue, setValue] = useState("1");
+  const [inputValue, setValue] = useState("reactjs");
   const [subimg, setSubreddit] = useState(inputValue);
   // Update the subreddit when the user presses enter
   const handleSubmit = e => {
